1265c97928980b185a9be08c59a8693e7b25d497
samuel ndembu <samuelndembu5@gmail.com>
2019-03-16 14:57:56 +0300

    conflict resolved

--- application/modules/schools/views/layouts/header.php
+++ application/modules/schools/views/layouts/header.php
@@ -6,5 +6,5 @@
 <link href="<?php echo base_url(); ?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,500,600,700" rel="stylesheet">
 <link href="<?php echo base_url(); ?>assets/themes/custom/css/custom.css" rel="stylesheet" type="text/css" media="screen">
-
+<link href="<?php echo base_url(); ?>assets/themes/custom/css/login.css" rel="stylesheet" type="text/css" media="screen">
 
