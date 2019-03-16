11af0a450c005a84751f618bf4822c30dcf8469f
Muraura <patricia.kanana@nanyukiappfactory.onmicrosoft.com>
2019-03-16 17:54:00 +0300

    auth module created

--- application/modules/schools/views/layouts/layout.php
+++ application/modules/schools/views/layouts/layout.php
@@ -1,40 +1,37 @@
-<!doctype html>
-<html lang="en">
+            <!doctype html>
+            <html lang="en">
 
-<head>
-    <?php $this->load->view('laikipiaschools/layouts/header');?>
-</head>
+            <head>
+                <?php $this->load->view('laikipiaschools/layouts/header');?>
+            </head>
 
-<body>
+            <body>
+            <div class="container-fluid">
+                    <div class="row">
+                    <main role="main" class="col-md-9 ml-sm-auto col-lg-10">
+                            <?php
+            $error = $this->session->flashdata('error');
+            $success = $this->session->flashdata('success');
+            if (!empty($error)) {
+                ?>
+                            <div class="alert alert-danger">
+                                <?php echo $error; ?>
+                            </div>
+                            <?php
+            }
+            if (!empty($success)) {
+                ?>
+                            <div class="alert alert-success">
+                                <?php echo $success; ?>
+                            </div>
+                            <?php
+            }
 
-
-    <div class="container-fluid">
-        <div class="row">
-            <?php $this->load->view('laikipiaschools/layouts/includes/sidebar');?>
-
-            <main role="main" class="col-md-9 ml-sm-auto col-lg-10">
-                <?php
-$error = $this->session->flashdata('error');
-$success = $this->session->flashdata('success');
-if (!empty($error)) {
-    ?>
-                <div class="alert alert-danger">
-                    <?php echo $error; ?>
+            echo $content;
+            ?>
+                        </main>
+                    </div>
                 </div>
-                <?php
-}
-if (!empty($success)) {
-    ?>
-                <div class="alert alert-success">
-                    <?php echo $success; ?>
-                </div>
-                <?php
-}
+                <?php $this->load->view('laikipiaschools/layouts/footer');?>
 
-echo $content;
-?>
-            </main>
-        </div>
-    </div>
-
-    
+                
