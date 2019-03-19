f9c6e3709a93353fae6546a037e6ce943b119160
Muraura <patricia.kanana@nanyukiappfactory.onmicrosoft.com>
2019-03-19 11:53:04 +0300

    template working well

--- application/modules/admin/views/layouts/header.php
+++ application/modules/admin/views/layouts/header.php
@@ -4,6 +4,5 @@
     <title><?php echo $title; ?></title>
     <link href="<?php echo base_url(); ?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="<?php echo base_url(); ?>assets/themes/custom/css/login.css" rel="stylesheet" type="text/css" media="screen">
-    <link href="<?php echo base_url(); ?>assets/themes/custom/css/dashboard.css" rel="stylesheet" type="text/css" media="screen">
 </head>
 
